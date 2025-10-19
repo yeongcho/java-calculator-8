e9dbba8b41ce9a9b9c1910e801d1bc9a6c9e9913
yeongcho <yeoncho17@naver.com>
2025-10-19 23:09:59 +0900

    feat: 4. 예외 처리  추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -13,12 +13,26 @@ public class StringCalculator {
         //기본 구분자(, 또는 :)를 기준으로 문자열 분리
         String[] characters = Parser.Default(inputValue);
 
-        //분리된 문자열을 정수로 변환 후 합산
-        for (String chracter : characters) {
-            sum += Integer.parseInt(chracter);
+        // 분리된 문자열을 정수로 변환 후 합산
+        for (String character : characters) {
+            try {
+                int number = Integer.parseInt(character);
+
+                //음수를 입력된 경우
+                if (number < 0) {
+                    throw new IllegalArgumentException("음수는 입력할 수 없습니다: " + number);
+                }
+
+                //유효한 숫자면 합계에 더하기
+                sum += number;
+
+            //숫자가 아닌 문자가 구분자외에 입력된 경우
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다: " + character);
+            }
         }
+
         //최종 합계 반환
         return sum;
     }
-
 }
