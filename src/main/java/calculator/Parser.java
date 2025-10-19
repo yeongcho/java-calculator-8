131dd4221ddce0796dac2ab86da65513ee572744
yeongcho <yeoncho17@naver.com>
2025-10-19 23:13:23 +0900

    fix: Parser.java의 기본 구분자 split 패턴 수정

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -8,7 +8,7 @@ public class Parser {
             return Custom(inputValue);
         }
         //기본 구분자(, 또는 :)를 기준으로 문자열을 분리
-        return inputValue.split(", | :");
+        return inputValue.split("[,:]");
     }
 
     //커스텀 구분자를 기준으로 입력 문자열을 분리
